d530694c38056861e7b6ecc4059043496bbdcff9
unknown <dj.zaragoza@icloud.com>
2020-06-20 11:12:36 -0700

    pushing up more code

--- src/src/components/Nav.js
+++ src/src/components/Nav.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import './Nav.css';
+import Dropdown from './Dropdown';
 
-const ModalRules = ({ children, show, setShow }) => {
-   const content = show && (
+const RulesModal = ({ children, showRules, setShowRules }) => {
+   const rulesContent = showRules && (
       <div className="overlay">
          <div className="modal">
             <button
                className="close-modal"
                type="button"
-               onClick={() => setShow(false)}
+               onClick={() => setShowRules(false)}
             >
                Close
             </button>
@@ -17,19 +17,38 @@ const ModalRules = ({ children, show, setShow }) => {
       </div>
    );
 
-   return content;
+   return rulesContent;
+};
+
+const InfoModal = ({ children, showInfo, setShowInfo }) => {
+   const infoContent = showInfo && (
+      <div className="overlay">
+         <div className="modal">
+            <button
+               className="close-modal"
+               type="button"
+               onClick={() => setShowInfo(false)}
+            >
+               Close
+            </button>
+            <div className="modal-body">{children}</div>
+         </div>
+      </div>
+   );
+
+   return infoContent;
 };
 
 function Nav() {
-   const [show, setShow] = useState(false);
-
+   const [showRules, setShowRules] = useState(false);
+   const [showInfo, setShowInfo] = useState(false);
    return (
       <div className="nav-wrapper">
-         <h1>John Conway's Game of Life</h1>
-         <button type="button" onClick={() => setShow(true)}>
-            Game Rules
+         <h1>Game of Life</h1>
+         <button type="button" onClick={() => setShowRules(true)}>
+            Rules of the game
          </button>
-         <ModalRules show={show} setShow={setShow}>
+         <RulesModal showRules={showRules} setShowRules={setShowRules}>
             <div>
                <h3>How it works</h3>
                <ol>
@@ -40,25 +59,37 @@ function Nav() {
                      Any live cell with two or three live neighbors lives on to the next generation.
                   </li>
                   <li>
-                     Any dead cell with exactly three live neighbors becomes a live cell due to reproduction.
+                     Any live cell with more than three live neighbors dies of overcrowding.
+                  </li>
+                  <li>
+                     Any live cell with exactly three live neighbors becomes a live cell due to reproduction.
                   </li>
                </ol>
                <sup>
-                  To find out more, visit Wikipedia: <a href="http://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules">Conway's Game of Life</a>
+                  Read more on Wikipedia: <a href="http://en.wikipedia.org/wiki/Conway's Game_of_Life#Rules">
+                     John Conway's Game of Life
+                  </a>
                </sup>
-               <h3>How to play</h3>
+               <h3>How to Play</h3>
                <ul>
-                  <li>Create a few cells by clicking on the grid.  FYI: if the cell has fewer than two living neighbor cells, it will die immediately in the next generation.  If your cell has more than three neighbors, it will die immediately in the next generation</li>
-                  <li>Click the <strong>Start</strong>button to watch the cells evolve automatically</li>
-                  <li>Click the <strong>Next</strong>button to watch the cell evolve one generation at a time</li>
-                  <li>Click the <strong>Stop</strong>button to pause the game</li>
-                  <li>Click the <strong>Clear</strong>button to clear your grid of all cells at any time.  This will also reset the generation counter to ZERO</li>
+                  <li>Create a few cells by clicking on the grid.  Remember: if the cell has fewer than two living neighbor cells, it will die immediately in the next generation.  If your cell has more than three neighbors, it will die immediately int he next generation</li>
+                  <li>Click the <strong>Start</strong> button to watch the cells evolve automatically</li>
+                  <li>Click the <strong>Next Generation</strong> button to watch the cell evolve one generation at a time.</li>
+                  <li>Click the <strong>Stop</strong> button to pause the game.</li>
+                  <li>Click the <strong>Clear</strong> butotn to clear the grid of all cells at any time.  This will reset the generation counter to ZERO.</li>
                </ul>
             </div>
-         </ModalRules>
-         <canvas />
+         </RulesModal>
+         <button onClick={() => setShowInfo(true)}>What do the buttons do?</button>
+         <InfoModal showInfo={showInfo} setShowInfo={setShowInfo}>
+            <div>
+               <h3>What do the buttons do?</h3>
+            </div>
+         </InfoModal>
+         <Dropdown />
       </div>
    );
 }
+
 
 export default Nav;
