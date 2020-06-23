7584a84bc3d2448e1761361c05324a972c151940
unknown <dj.zaragoza@icloud.com>
2020-06-23 09:00:38 -0700

    modified dropdown.js file

--- src/src/components/Dropdown.js
+++ src/src/components/Dropdown.js
@@ -12,4 +12,23 @@ function DropDown() {
    );
 }
 
+{/* <button
+   onClick={() => {
+      const clearedGrid = [];
+      for (let i = 0; i < rows; i++) {
+         clearedGrid.push(
+            Array.from(Array(cols), () => (Math.() > .7 ? 1 : 0))
+         );
+      }
+
+      if (activeGrid === 1) {
+         setFrameOne(clearedGrid);
+      } else {
+         setFrameTwo(clearedGrid);
+      }
+   }}
+>
+   Glider
+</button> */}
+
 export default DropDown;
